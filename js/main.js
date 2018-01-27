ebfb9c642e364f8b1f4706db8da0e8dba05d6ea7
Jared Truscott <Jtruscott78@gmail.com>
2018-01-27 05:48:38 -0500

    added gallery layout

---
+++ js/main.js
@@ -0,0 +1,29 @@
+var galleryController = (function(){
+//
+
+
+
+}());
+
+
+
+
+
+
+
+
+
+
+
+/* <div class="container-fluid">
+<div class="row text-center">
+<div class="col-lg-3 col-md-4 col-xs-6">
+<a href="#" class="d-block mb-4 h-100">
+<img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt="">
+</a>
+</div>
+<div class="col-lg-3 col-md-4 col-xs-6">
+<a href="#" class="d-block mb-4 h-100">
+<img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt="">
+</a>
+</div> */
